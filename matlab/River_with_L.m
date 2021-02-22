a1e3605723e998e60574809acc3d41e513695e1d
Bart Vermeulen <bart.vermeulen@wur.nl>
2021-02-22 15:19:56 +0100

    Added solution to exercise with classes

---
+++ matlab/River_with_L.m
@@ -0,0 +1,19 @@
+classdef River < handle
+    properties(Constant)
+        g = 9.81
+    end
+    properties
+        slope = 1e-4
+        discharge = 1000
+        chezy = 50
+        L = 10000
+    end
+    methods
+        function out = critical_slope ( obj )
+            out = obj.g / obj.chezy ^2;
+        end
+        function out=bed_elevation( obj )
+            out = linspace(0, L , 15);
+        end
+    end
+end
