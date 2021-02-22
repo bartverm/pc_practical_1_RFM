f907a435c04996ec05725b418ec81892bb8d8dfb
Bart Vermeulen <bart.vermeulen@wur.nl>
2021-02-22 16:32:52 +0100

    adjusted River.m

--- matlab/River.m
+++ matlab/River.m
@@ -6,10 +6,14 @@ classdef River < handle
         slope = 1e-4
         discharge = 1000
         chezy = 50
+        L = 10000
     end
     methods
         function out = critical_slope ( obj )
             out = obj.g / obj.chezy ^2;
+        end
+        function out=bed_elevation( obj )
+            out = linspace(0, obj.L , 15);
         end
     end
 end
