bd22522620e31e5d7186e88bf9adce79a070b0f0
Bart Vermeulen <bart.vermeulen@wur.nl>
2021-02-25 15:55:44 +0100

    adding figure of example

--- matlab/river_engineering_exercises.m
+++ matlab/river_engineering_exercises.m
@@ -14,3 +14,17 @@ B(1).a0=B(2).a_equilibrium; % Set bounday condition of upstream end to equilibri
 B(1).x_end=B(1).x_target; % Set the end of the upstream reach to the estimated end of the backwater
 B(1).zb0=B(2).bed_level(end); % Adjust the inital bed level of the upstream reach
 plot(B) % plot the backwater objects
+
+%% Roughness transition
+clearvars
+B(2)=Backwater; % create two backwater objects
+[B(:).So]=deal(5e-3); % set the slope for the two reaches
+B(1).Chez=65; % set roughness for upstream reach
+B(2).Chez=55; % set roughness for downstream reach
+B(1).a0=B(1).a_equilibrium; % Set the downstream reach to equilibrium
+B(1).x_end=200; % Set end of computation to positive 
+B(2).x0=B(1).x_end; % Set start of downstream reach to match end of upstream reach
+B(2).a0=B(1).a_equilibrium; % Set bounday condition of donwstream reach to equilibrium depth of upstream end
+B(2).x_end=B(2).x_target; % Set the end of the downstream reach to the estimated end of the backwater
+B(2).zb0=B(1).bed_level(end); % Adjust the inital bed level of the downstream reach
+plot(B) % plot the backwater objects
